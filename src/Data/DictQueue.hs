0f0f81eb47d2c5610b3a63f8646d77cb208ffdd3
Mark Wotton <mwotton@gmail.com>
2014-02-02 09:40:55 +1100

    add basic tests

--- src/Data/DictQueue.hs
+++ src/Data/DictQueue.hs
@@ -1,1 +1,13 @@
 module Data.DictQueue where
+
+data DictQueue a = DictQueue ()
+
+extractMin :: Eq a => DictQueue a -> Maybe (a, DictQueue a)
+extractMin = undefined
+
+
+insert :: Eq a => DictQueue a -> a -> DictQueue a
+insert = undefined
+
+empty :: DictQueue a
+empty = undefined
--- test/Data/DictQueueSpec.hs
+++ test/Data/DictQueueSpec.hs
@@ -1,1 +1,12 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Data.DictQueueSpec where
+
+import           Data.DictQueue
+import           Data.List                (foldl', unfoldr)
+import           Test.Hspec
+import           Test.QuickCheck.Property
+
+spec = describe "DictQueue" $ do
+  it "should behave as a queue" $ do
+    property $ \(xs :: [Int]) -> unfoldr extractMin (foldl'  insert empty xs) == xs
